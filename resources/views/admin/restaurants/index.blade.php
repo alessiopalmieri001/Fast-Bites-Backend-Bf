2e7b8e88933b88297da631c131dd7e22b3ac1f95
alessiopalmieri001 <alessiopalmieri001@gmail.com>
2024-03-27 17:41:21 +0100

    add style

--- resources/views/admin/restaurants/index.blade.php
+++ resources/views/admin/restaurants/index.blade.php
@@ -5,19 +5,19 @@
 @section('main-content')
     <section>
         <div class="row">
+            <h1 class="text-center m-4 text-light">I nostri Ristoranti</h1>
             @foreach ($restaurants as $restaurant)
-                <div class="col-12">
-                    <div class="card m-1">
-                        <h1>
-                            {{ $restaurant->name }}
-                        </h1>
-                        <h1>
-                            {{ $restaurant->address }}
-                        </h1>
-                        <p>
-                            {{ $restaurant->user_id }}
-                        </p>
-                        
+                <div class="col-md-4 container">
+                    <div class="card m-1 custom-card" >
+                        <div class="card-content">
+                            <div class="card-img-container">
+                                <img src="{{ $restaurant['img'] }}" alt="{{ $restaurant->name }}">
+                            </div>
+                            <div class="card-details">
+                                <h2>{{ $restaurant->name }}</h2>
+                                <h4>{{ $restaurant->address }}</h4>
+                            </div>
+                        </div>
                     </div>
                 </div>
             @endforeach
