11e0a62a62b4de3aaab469f36a3e6515f68c402f
GiuseppeF11 <faillagiuseppe13@gmail.com>
2024-04-02 11:34:32 +0200

    Add all references restaurant-category

--- database/seeders/RestaurantCategorySeeder.php
+++ database/seeders/RestaurantCategorySeeder.php
@@ -47,12 +47,68 @@ class RestaurantCategorySeeder extends Seeder
                     "category" => 4
                 ],
                 [
+                    "restaurant" => 5,      //BAKERY AND DREAM
+                    "category" => 5
+                ],
+                [
+                    "restaurant" => 6,      //PIZZIUM
+                    "category" => 1
+                ],
+                [
+                    "restaurant" => 6,
+                    "category" => 6
+                ],
+                [
+                    "restaurant" => 7,      //BENTOTECA
+                    "category" => 4
+                ],
+                [
+                    "restaurant" => 8,      //PASTICCERIA DA GELSOMINA
+                    "category" => 5
+                ],
+                [
+                    "restaurant" => 9,      //BURGER KING
+                    "category" => 4
+                ],
+                [
+                    "restaurant" => 9,      
+                    "category" => 8
+                ],
+                [
                     "restaurant" => 10,      //TAKUMI
                     "category" => 3
                 ],
                 [
                     "restaurant" => 11,      //GINMI
                     "category" => 2
+                ],
+                [
+                    "restaurant" => 12,      //LIEVITA'
+                    "category" => 1
+                ],
+                [
+                    "restaurant" => 12,    
+                    "category" => 6
+                ],
+                [
+                    "restaurant" => 13,      //ISHI FUSION
+                    "category" => 3
+                ],
+                [
+                    "restaurant" => 14,      //FIVE GUYS
+                    "category" => 4
+                ],
+                [
+                    "restaurant" => 14,      //FIVE GUYS
+                    "category" => 8
+                ],
+                [
+                    "restaurant" => 15,      //PAN
+                    "category" => 7
+                ],
+                [
+                    "restaurant" => 15,      
+                    "category" => 1
                 ],
             ];
 
