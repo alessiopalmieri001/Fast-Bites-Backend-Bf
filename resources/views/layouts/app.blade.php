d53052ecee38f2e1db5fda7a9f009a9192b561f2
Soniamy <marianna.marzullo@gmail.com>
2024-04-02 10:21:22 +0200

    Add style welcome

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,8 +4,9 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-
         <title>@yield('page-title') | {{ config('app.name', 'Laravel') }}</title>
+        <link href="{{ asset('vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet">
+
 
         @vite('resources/js/app.js')
     </head>
