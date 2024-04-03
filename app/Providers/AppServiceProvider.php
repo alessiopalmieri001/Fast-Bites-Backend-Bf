fc35b584b535fc4b02e8b42c32bd098491362d75
costanzaguala <costanzasilviaguala@gmail.com>
2024-04-03 18:30:00 +0200

    tolto il tuo menu e orders header

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Auth;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Using Closure based composers...
+        View::composer('layouts.app', function ($view) {
+            $user = Auth::user();
+            $view->with('user', $user);
+        });
     }
 }
