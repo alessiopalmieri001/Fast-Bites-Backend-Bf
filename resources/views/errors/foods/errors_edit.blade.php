cb508ff238b5e77bdb3fe16ca12705dbfa6fe267
Soniamy <marianna.marzullo@gmail.com>
2024-04-03 23:28:53 +0200

    fix foods_errors_edit

--- resources/views/errors/foods/errors_edit.blade.php
+++ resources/views/errors/foods/errors_edit.blade.php
@@ -4,7 +4,7 @@
 
             <div class="pt-5 d-flex flex-column align-items-center justify-content-center">
                 <h1 class="text-center">Oh no! Non puoi modificare questo piatto</h1>
-                <a href="{{ route('admin.restaurants.create') }}" class="btn btn-primary mt-3 d-flex align-items-center"><i
+                <a href="{{ route('admin.foods.index') }}" class="btn btn-primary mt-3 d-flex align-items-center"><i
                         class="fa-solid fa-circle-left me-3 fs-5"></i><span> Torna ai miei piatti</span></a>
             </div>
 
