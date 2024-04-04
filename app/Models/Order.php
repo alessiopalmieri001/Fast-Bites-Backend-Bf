e70ebf4f47637af8f9a890671ff1a5414fb2c1db
Soniamy <marianna.marzullo@gmail.com>
2024-04-04 18:53:27 +0200

    Add API

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -5,18 +5,29 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+//model
+use App\Models\Food;
+
 class Order extends Model
 {
     use HasFactory;
 
     protected $fillable = [
+        'name',
+        'surname',
+        'email',
+        'phone_num',
+        'address',
         'total',
         'status',
-        'name',
-        'email',
-        'address',
-        'phone_num',
     ];
+
+
+    //Relazione 1 to many (Order -> Restaurant)
+    public function restaurant()
+    {
+        return $this->belongsTo(Restaurant::class);
+    }
 
     //Relazione many to many (Order -> Food)
     public function foods()
@@ -24,10 +35,5 @@ class Order extends Model
         return $this->belongsToMany(Food::class)->withPivot('quantity')->withTrashed();
     }
 
-    //Relazione 1 to many (Order -> Restaurant)
-    public function restaurant()
-    {
-        return $this->belongsTo(Restaurant::class);
-    }
 
 }
