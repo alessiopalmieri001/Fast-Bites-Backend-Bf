b813eefd30cfb34a413fd5c8ee7a87230cac1eb6
alessiopalmieri001 <alessiopalmieri001@gmail.com>
2024-04-15 15:16:14 +0200

    add charts.js

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -12,18 +12,48 @@
             <h4 class="subtitle-2 text-center">
                 Benvenuto nella tua dashboard.
             </h4>
+            <canvas id="myChart"></canvas>
         </div>
     </div> 
 </div>
+
+
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+<script>
+    const ctx = document.getElementById('myChart');
+
+    new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+        y: {
+            beginAtZero: true
+        }
+        }
+    }
+    });
+</script>
+
+</script>
 @endsection
+
+
 
 <style lang="scss" scoped>
     .title {
-      font-family: 'Paytone One', sans-serif;
-      font-size: 3rem;
-      color: white;
-      text-align: center;
-      margin-bottom: 20px;
+        font-family: 'Paytone One', sans-serif;
+        font-size: 3rem;
+        color: white;
+        text-align: center;
+        margin-bottom: 20px;
     }
     
     .subtitle-2 {
